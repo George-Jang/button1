100a1b4e2818700d3328438720a5637089980501
gwcos <gwcos60@naver.com>
2023-09-11 14:36:17 +0900

    commit

--- button1.py
+++ button1.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Jang Dong Hyun")
+b1 = Button(window, text="Jang DongHyun")
 b2 = Button(window, text="60172188")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
